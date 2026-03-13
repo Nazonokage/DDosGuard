79d73cd398fe3701e92e34c7feb46d625f04d991
Joshua Valencia <149948627+Nazonokage@users.noreply.github.com>
2026-03-13 11:48:31 +0800

    Update axios baseURL for production environment

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // ── LOCAL (Flask running on XAMPP machine) ─────────────────────────────────
-  baseURL: 'http://localhost:5000',
+  //baseURL: 'http://localhost:5000',
 
   // ── PRODUCTION (PythonAnywhere) ── uncomment when deploying ───────────────
-  // baseURL: 'https://yourusername.pythonanywhere.com',
+   baseURL: 'https://ddosguard-7zrx.onrender.com/',
 
   withCredentials: false,
 });
